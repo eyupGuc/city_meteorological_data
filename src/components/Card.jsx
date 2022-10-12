8caac666d3ebd62b1ee5a83a93274f2907f0b305
eyupGuc <meydanguresi@gmail.com>
2022-10-12 10:22:17 +0300

    v

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,10 @@
-// import adana from "../img/adana.jpeg";
-// import agri from "../img/agri.jpeg";
-// import ankara from "../img/ankara.jpg";
-// import gaziantep from "../img/gaziantep.jpg";
-// import igdir from "../img/igdir.jpg";
-// import istanbul from "../img/istanbul.jpg";
-// import izmir from "../img/izmir.jpg";
-// import rize from "../img/rize.jpg";
-// import sanliurfa from "../img/adana.jpg";
+
 const Card=({data})=>{
-    console.log(data);
+    // console.log(data);
     return (<div className="card-container">
         {data.map((item)=>{
 const{id,city,highestTemperature,lowestTemperature,maxSnowThickness,totalPrecipitation}=data;
-return( <img src={item.img} alt="img"/>)
+return( <div key={id}><img src={item.img} alt="img"/> <h1>{item.city}</h1></div>)
         })}
         
 
--- src/data.js
+++ src/data.js
@@ -1,43 +1,48 @@
 const data=[
-    // {
-    //     id:1,
-    //     city:"Ankara",
-    //     totalPrecipitation:"392mm",
-    //     lowestTemperature:"-24.9",
-    //     highestTemperature:"41.0",
-    //     maxSnowThickness:"33cm"
+    {
+        id:1,
+        city:"Ankara",
+        totalPrecipitation:"392mm",
+        lowestTemperature:"-24.9",
+        highestTemperature:"41.0",
+        maxSnowThickness:"33cm",
+        img:"https://www.mgm.gov.tr/site/ex_mgmGununFotografi.aspx?y=2021&a=08&g=14"
         
 
-    // },
-    // {
-    //     id:2,
-    //     city:"Gaziantep",
-    //     totalPrecipitation:"566mm",
-    //     lowestTemperature:"-17.5",
-    //     highestTemperature:"44.0",
-    //     maxSnowThickness:"100cm"
+    },
+    {
+        id:2,
+        city:"Gaziantep",
+        totalPrecipitation:"566mm",
+        lowestTemperature:"-17.5",
+        highestTemperature:"44.0",
+        maxSnowThickness:"100cm",
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Gaziantep_castle.JPG/800px-Gaziantep_castle.JPG"
         
 
-    // }, {
-    //     id:3,
-    //     city:"İstanbul",
-    //     totalPrecipitation:"648mm",
-    //     lowestTemperature:"-12.6",
-    //     highestTemperature:"39.5",
-    //     maxSnowThickness:"60cm"
+    }, {
+        id:3,
+        city:"İstanbul",
+        totalPrecipitation:"648mm",
+        lowestTemperature:"-12.6",
+        highestTemperature:"39.5",
+        maxSnowThickness:"60cm",
+        img:"https://www.mgm.gov.tr/site/ex_mgmGununFotografi.aspx?y=2008&a=02&g=05"
+
         
 
-    // }, {
-    //     id:4,
-    //     city:"İzmir",
-    //     totalPrecipitation:"713mm",
-    //     lowestTemperature:"-8.2",
-    //     highestTemperature:"43.0",
-    //     maxSnowThickness:"32cm"
+    }, {
+        id:4,
+        city:"İzmir",
+        totalPrecipitation:"713mm",
+        lowestTemperature:"-8.2",
+        highestTemperature:"43.0",
+        maxSnowThickness:"32cm",
+        img:"https://www.mgm.gov.tr/site/ex_mgmGununFotografi.aspx?y=2008&a=03&g=09",
         
 
-    // }, {
-       { id:5,
+    }, {
+        id:5,
         city:"Adana",
         totalPrecipitation:"668mm",
         lowestTemperature:"-8.1",
@@ -47,44 +52,48 @@ const data=[
         
 
     }, 
-    // {
-    //     id:6,
-    //     city:"Şanlıurfa",
-    //     totalPrecipitation:"460mm",
-    //     lowestTemperature:"-12.4",
-    //     highestTemperature:"46.8",
-    //     maxSnowThickness:"29cm"
+    {
+        id:6,
+        city:"Şanlıurfa",
+        totalPrecipitation:"460mm",
+        lowestTemperature:"-12.4",
+        highestTemperature:"46.8",
+        maxSnowThickness:"29cm",
+        img:"https://www.goller.gen.tr/images/sanliurfa-balikli-gol.jpg",
         
 
-    // }, {
-    //     id:7,
-    //     city:"Ağrı",
-    //     totalPrecipitation:"524mm",
-    //     lowestTemperature:"-45.6",
-    //     highestTemperature:"39.9",
-    //     maxSnowThickness:"225cm"
+    }, {
+        id:7,
+        city:"Ağrı",
+        totalPrecipitation:"524mm",
+        lowestTemperature:"-45.6",
+        highestTemperature:"39.9",
+        maxSnowThickness:"225cm",
+    img:"https://www.mgm.gov.tr/site/ex_mgmGununFotografi.aspx?y=2008&a=02&g=29"
         
 
-    // }, {
-    //     id:8,
-    //     city:"Rize",
-    //     totalPrecipitation:"2302mm",
-    //     lowestTemperature:"-7.0",
-    //     highestTemperature:"38.2",
-    //     maxSnowThickness:"187cm"
+    }, {
+        id:8,
+        city:"Rize",
+        totalPrecipitation:"2302mm",
+        lowestTemperature:"-7.0",
+        highestTemperature:"38.2",
+        maxSnowThickness:"187cm",
+        img:"https://www.mgm.gov.tr/site/ex_mgmGununFotografi.aspx?y=2022&a=10&g=09"
         
 
-    // },
-    // {
-    //     id:9,
-    //     city:"Iğdır",
-    //     totalPrecipitation:"259mm",
-    //     lowestTemperature:"-30.3",
-    //     highestTemperature:"42.0",
-    //     maxSnowThickness:"38cm"
+    },
+    {
+        id:9,
+        city:"Iğdır",
+        totalPrecipitation:"259mm",
+        lowestTemperature:"-30.3",
+        highestTemperature:"42.0",
+        maxSnowThickness:"38cm",
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/I%C4%9Fd%C4%B1r_ve_A%C4%9Fr%C4%B1_Da%C4%9F%C4%B1.jpg/1200px-I%C4%9Fd%C4%B1r_ve_A%C4%9Fr%C4%B1_Da%C4%9F%C4%B1.jpg",
         
 
-    // },
+    },
 ];
 
 export default data;
