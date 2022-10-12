1d2db1aefe9430cb6a24f37012afe6182e1430b5
eyupGuc <meydanguresi@gmail.com>
2022-10-12 03:34:48 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+const data=[
+    {
+        id:1,
+        city:"Ankara",
+        yıllık_toplam_yağış:"392mm",
+        en_düşük_sıcaklık:"-24.9",
+        en_yüksek_sıcaklık:"41.0",
+        max_kar_kalınılığı:"33cm"
+        
+
+    },
+    {
+        id:2,
+        city:"Gaziantep",
+        yıllık_toplam_yağış:"566mm",
+        en_düşük_sıcaklık:"-17.5",
+        en_yüksek_sıcaklık:"44.0",
+        max_kar_kalınılığı:"100cm"
+        
+
+    }, {
+        id:3,
+        city:"İstanbul",
+        yıllık_toplam_yağış:"648mm",
+        en_düşük_sıcaklık:"-12.6",
+        en_yüksek_sıcaklık:"39.5",
+        max_kar_kalınılığı:"60cm"
+        
+
+    }, {
+        id:4,
+        city:"İzmir",
+        yıllık_toplam_yağış:"713mm",
+        en_düşük_sıcaklık:"-8.2",
+        en_yüksek_sıcaklık:"43.0",
+        max_kar_kalınılığı:"32cm"
+        
+
+    }, {
+        id:5,
+        city:"Adana",
+        yıllık_toplam_yağış:"668mm",
+        en_düşük_sıcaklık:"-8.1",
+        en_yüksek_sıcaklık:"45.6",
+        max_kar_kalınılığı:"1cm"
+        
+
+    }, {
+        id:6,
+        city:"Şanlıurfa",
+        yıllık_toplam_yağış:"460mm",
+        en_düşük_sıcaklık:"-12.4",
+        en_yüksek_sıcaklık:"46.8",
+        max_kar_kalınılığı:"29cm"
+        
+
+    }, {
+        id:7,
+        city:"Ağrı",
+        yıllık_toplam_yağış:"524mm",
+        en_düşük_sıcaklık:"-45.6",
+        en_yüksek_sıcaklık:"39.9",
+        max_kar_kalınılığı:"225cm"
+        
+
+    }, {
+        id:8,
+        city:"Rize",
+        yıllık_toplam_yağış:"2302mm",
+        en_düşük_sıcaklık:"-7.0",
+        en_yüksek_sıcaklık:"38.2",
+        max_kar_kalınılığı:"187cm"
+        
+
+    },
+];
